75d2032d49d13060c65bcd194c0e4a73b8d881da
Nagi-ei <nakt4453@gmail.com>
2025-03-16 23:46:42 +0900

    👷 Add github actions for dev
    add develop branch

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <>
       <h1 className="text-3xl font-bold">Hello, World</h1>
-      <h2>Main Branch</h2>
+      <h2>Develop Branch</h2>
       <Button onClick={() => setCount(count + 1)}>Count +1</Button>
       <p>{count}</p>
     </>
