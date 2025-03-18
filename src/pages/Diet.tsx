682118a07eff46ab0e5aeb86793049695d1a1104
Nagi-ei <nakt4453@gmail.com>
2025-03-18 15:21:12 +0900

    ✨ Implement modal component
    common component

--- src/pages/Diet.tsx
+++ src/pages/Diet.tsx
@@ -1,8 +1,20 @@
+import Modal from '@/components/Modal';
+import { Button } from '@/components/ui/button';
+import useModal from '@/states/modal';
+
 export default function Diet() {
+  // 모달 사용 예시
+  const { openModal, closeModal } = useModal();
+
   return (
     <main className="flex justify-center pl-[200px]">
       <section className="max-w-5xl">
         <h2>조건 넣고 식단 추천받기</h2>
+        <Button onClick={openModal}>모달 열기</Button>
+        <Modal>
+          <p>모달!</p>
+          <Button onClick={closeModal}>모달 닫기</Button>
+        </Modal>
       </section>
     </main>
   );
