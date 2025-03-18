4ea1097c88d2c69cb7e734255a66fd4a3eb64120
Nagi-ei <nakt4453@gmail.com>
2025-03-19 02:51:14 +0900

    ✨ Implement routing
    admin and report

---
+++ src/pages/admin/AdminAi.tsx
@@ -0,0 +1,10 @@
+export default function AdminAi() {
+  return (
+    <main className="flex justify-center pl-[200px]">
+      <section className="max-w-5xl">
+        <h2>관리자 페이지</h2>
+        <h3>ai 로그</h3>
+      </section>
+    </main>
+  );
+}
---
+++ src/pages/admin/AdminReports.tsx
@@ -0,0 +1,10 @@
+export default function AdminReports() {
+  return (
+    <main className="flex justify-center pl-[200px]">
+      <section className="max-w-5xl">
+        <h2>관리자 페이지</h2>
+        <h3>모든 문의 사항 목록</h3>
+      </section>
+    </main>
+  );
+}
