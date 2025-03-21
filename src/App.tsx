77713e820857a27520fbbdb5c785bc68811a4812
kim-yuju <revelation0829@naver.com>
2025-03-21 15:35:45 +0900

    🚚 파일 이동
    user파일에 옮겨두었습니다

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import { Routes, Route } from 'react-router';
 import Layout from './components/main/Layout';
-import SignIn from './pages/SignIn';
-import FindId from './pages/FindId';
-import FindPw from './pages/FindPw';
-import Profile from './pages/Profile';
+import SignIn from './pages/user/SignIn';
+import FindId from './pages/user/FindId';
+import FindPw from './pages/user/FindPw';
+import Profile from './pages/user/Profile';
 import Admin from './pages/admin/Admin';
 import NotFound from './pages/NotFound';
 import Recipe from './pages/main/Recipe';
@@ -19,8 +19,8 @@ import AdminLayout from './components/main/AdminLayout';
 import AdminAi from './pages/admin/AdminAi';
 import AdminUsers from './pages/admin/AdminUsers';
 import AdminReports from './pages/admin/AdminReports';
-import SignUpHandler from './pages/SignUpHandler';
-import SignUp from './pages/signUp';
+import SignUpHandler from './pages/user/SignUpHandler';
+import SignUp from './pages/user/signUp';
 
 function App() {
   return (
