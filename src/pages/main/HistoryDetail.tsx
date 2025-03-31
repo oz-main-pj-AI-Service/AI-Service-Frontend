2993f8953f93d00f103c6067119267aef142dd49
Nagi-ei <nakt4453@gmail.com>
2025-03-31 18:40:39 +0900

    ✨ Implement admin users

--- src/pages/main/HistoryDetail.tsx
+++ src/pages/main/HistoryDetail.tsx
@@ -1,6 +1,6 @@
-import { useParams } from 'react-router';
 import { formatDateYMD } from '@/lib/utils';
 import { History } from '@/types/ai';
+import { useParams } from 'react-router';
 
 export default function HistoryDetail() {
   const { id } = useParams() as { id: string };
