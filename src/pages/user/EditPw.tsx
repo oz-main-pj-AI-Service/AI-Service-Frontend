2293008bf9ced77d9701af17d2309d796f3bec63
kim-yuju <revelation0829@naver.com>
2025-04-02 02:41:17 +0900

    아이디 비밀번호 찾기

--- src/pages/user/EditPw.tsx
+++ src/pages/user/EditPw.tsx
@@ -1,23 +1,87 @@
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
+import logo_black from '@/assets/logo_black.png';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+import axios from 'axios';
+import { API_URL } from '@/constants/url';
+import { useEffect, useState } from 'react';
 
-//이메일에 인증 완료되면 여기로 이동하게 만들어야 함
+const schema = z
+  .object({
+    password1: z
+      .string()
+      .min(8, { message: '비밀번호는 최소 8자 이상입니다' })
+      .regex(/^(?=.*[!@#$%^&*()_+=-{};:'<>,./?]).*$/, {
+        message: '비밀번호는 최소 하나의 특수문자를 포함해야 합니다',
+      }),
+    password2: z
+      .string()
+      .min(8, { message: '비밀번호는 최소 8자 이상입니다' })
+      .regex(/^(?=.*[!@#$%^&*()_+=-{};:'<>,./?]).*$/, {
+        message: '비밀번호는 최소 하나의 특수문자를 포함해야 합니다',
+      }),
+  })
+  .refine((data) => data.password1 === data.password2, {
+    message: '비밀번호가 일치하지 않습니다',
+    path: ['password2'],
+  });
+
 export default function EditPw() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(schema),
+  });
+
+  const [email, setEmail] = useState<string | null>(null);
+
+  useEffect(() => {
+    const url = window.location.href;
+    const queryParams = new URLSearchParams(new URL(url).search);
+    setEmail(queryParams.get('email'));
+  }, []);
+
+  const onSubmit = async (data: { password1: string; password2: string }) => {
+    try {
+      const requestData = { ...data, email };
+      const response = await axios.post(`${API_URL}/change-pw/`, requestData);
+      if (response.status === 200) {
+        console.log(response);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
-    <main className="flex flex-col items-center justify-center pl-[200px]">
-      <section className="flex max-w-5xl flex-col gap-4">
-        <img src="" alt="한상비서로고" />
-        <h1>비밀번호 변경</h1>
-        <label>
-          새 비밀번호
-          <Input type="password" placeholder="새 비밀번호" />
-        </label>
-        <label>
-          새 비밀번호 확인
-          <Input type="password" placeholder="새 비밀번호 확인" />
-        </label>
-        <Button>변경 완료</Button>
+    <main className="flex h-full w-full flex-col overflow-y-auto pt-14 pl-[200px]">
+      <section className="flex h-screen items-center justify-center">
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="mb-4 flex w-96 flex-col gap-4 rounded bg-white px-8 pt-6 pb-8 shadow-md"
+        >
+          <h1>
+            <img src={logo_black} alt="한상비서로고" />
+          </h1>
+          <h1>비밀번호 변경</h1>
+          <label>
+            새 비밀번호
+            <Input {...register('password1')} type="password" placeholder="새 비밀번호" />
+            {errors.password1 && <p className="text-xs text-red-500">{errors.password1.message}</p>}
+          </label>
+          <label>
+            새 비밀번호 확인
+            <Input type="password" {...register('password2')} placeholder="새 비밀번호 확인" />
+          </label>
+          {errors.password2 && <p className="text-xs text-red-500">{errors.password2.message}</p>}
+          <Button type="submit">변경 완료</Button>
+        </form>
       </section>
     </main>
   );
 }
+
+// change-pw/ 여기로 password1, password2 , 쿼리에서 뽑은 이메일 보내기
--- src/pages/user/FindPw.tsx
+++ src/pages/user/FindPw.tsx
@@ -1,19 +1,60 @@
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
+import logo_black from '@/assets/logo_black.png';
+import { z } from 'zod';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { API_URL } from '@/constants/url';
+import axios from 'axios';
+
+const schema = z.object({
+  email: z.string().email({ message: '유효하지 않은 이메일 형식입니다.' }),
+});
 
 export default function FindPw() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(schema),
+  });
+
+  const onSubmit = async (data: { email: string }) => {
+    try {
+      const response = await axios.post(`${API_URL}/user/find-password/`, {
+        email: data.email,
+      });
+      if (response.status === 200) {
+        console.log(response);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
-    <main className="flex flex-col items-center justify-center pt-[100px] pl-[200px]">
-      <section className="flex max-w-5xl flex-col gap-4">
-        <img src="" alt="한상비서로고" />
-        <h1>비밀번호 찾기</h1>
-        <label>
-          이메일
-          <Input type="email" placeholder="hansang@example.com" />
-        </label>
-        <Button>본인 인증</Button>
+    <main className="flex h-full w-full flex-col overflow-y-auto pt-14 pl-[200px]">
+      <section className="flex h-screen items-center justify-center">
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="mb-4 flex w-96 flex-col gap-4 rounded bg-white px-8 pt-6 pb-8 shadow-md"
+        >
+          <h1>
+            <img src={logo_black} alt="한상비서로고" />
+          </h1>
+          <h1>비밀번호 찾기</h1>
+          <label>
+            이메일
+            <Input type="email" {...register('email')} placeholder="hansang@example.com" />
+          </label>
+          {errors.email && <p style={{ color: 'red' }}>{errors.email.message}</p>}
+          <Button type="submit">본인 인증</Button>
+        </form>
       </section>
     </main>
   );
 }
 //모달로 이메일을 확인해주세요 띄우기
+
+// api.hansang.ai.kr/api/user/find-password/
+// 유저한테서 받은 이메일 보낼곳
