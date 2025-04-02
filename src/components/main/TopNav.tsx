bae12405417ea90b01f281421098f74d927d5dc0
Nagi-ei <nakt4453@gmail.com>
2025-04-02 16:02:57 +0900

    🐛 Fix report and delete login temp

--- src/components/main/TopNav.tsx
+++ src/components/main/TopNav.tsx
@@ -1,12 +1,10 @@
 import { Link } from 'react-router';
-import { Button } from '../ui/button';
-import { useUserTokenTemp } from '@/hooks/useUserTokenTemp';
-import { loginApiTemp } from '@/api/loginApiTemp';
 import { handleLogout } from '@/pages/user/handleLogout';
 import { useAuthStore } from '@/stores/authStore';
 import { Switch } from '../ui/switch';
 import { Label } from '../ui/label';
 import useDarkMode from '@/stores/darkmode';
+import { Button } from '../ui/button';
 
 export default function TopNav() {
   const { accessToken } = useAuthStore();
@@ -14,10 +12,6 @@ export default function TopNav() {
 
   // 임시 다크모드
   const { isDarkMode, toggleDarkMode } = useDarkMode();
-
-  // 임시 로그인
-  const { refetch: refetchUserToken } = useUserTokenTemp('user');
-  const { refetch: refetchAdminToken } = useUserTokenTemp('admin');
 
   return (
     <div className="fixed top-0 z-1 flex w-full items-center justify-end gap-4 bg-[var(--bg-light)] px-8 py-4 dark:bg-[var(--bg-dark)]">
@@ -27,40 +21,10 @@ export default function TopNav() {
         <Label htmlFor="dark-mode">다크 모드 {isDarkMode ? '🌙' : '☀️'}</Label>
       </div>
 
-      {/* 임시 로그인 */}
-      {accessToken ? (
-        <Button
-          onClick={() => {
-            loginApiTemp.logOut();
-          }}
-        >
-          로그아웃
-        </Button>
-      ) : (
-        <>
-          <Button
-            onClick={() => {
-              console.log('관리자 로그인');
-              refetchAdminToken();
-            }}
-          >
-            관리자 로그인
-          </Button>
-          <Button
-            onClick={() => {
-              console.log('유저 로그인');
-              refetchUserToken();
-            }}
-          >
-            임시 로그인
-          </Button>
-        </>
-      )}
-
       {accessToken ? (
         <>
           <Link to="/profile">회원정보</Link>
-          <button onClick={handleLogout}>로그아웃</button>
+          <Button onClick={handleLogout}>로그아웃</Button>
         </>
       ) : (
         <>
