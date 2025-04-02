33dc646b76caf4cb8a22cec7cdcbc871bcd5f070
kim-yuju <revelation0829@naver.com>
2025-04-02 21:37:14 +0900

    💄 User css
    유저부분 css 수정함

--- src/pages/user/FindId.tsx
+++ src/pages/user/FindId.tsx
@@ -49,14 +49,23 @@ export default function FindId() {
           <h1>
             <img src={logo_black} alt="한상비서로고" />
           </h1>
-          <h1>아이디 찾기</h1>
-          <label>
+          <label className="text-sm font-medium">
             휴대폰 번호
-            <Input type="tel" {...register('phone_number')} placeholder="숫자만 입력하세요" />
+            <Input
+              type="tel"
+              {...register('phone_number')}
+              placeholder="가입 시 입력한 휴대폰 번호를 입력하세요"
+            />
           </label>
-          {errors.phone_number && <p style={{ color: 'red' }}>{errors.phone_number.message}</p>}
-          <Button type="submit">본인인증</Button>
-          <Link to="/sign-in/find-pw">비밀번호 찾기</Link>
+          {errors.phone_number && (
+            <p className="text-xs text-red-500">{errors.phone_number.message}</p>
+          )}
+          <Button type="submit" className="text-white dark:bg-[var(--point-orange)]">
+            아이디 찾기
+          </Button>
+          <Link to="/sign-in/find-pw" className="text-right text-xs">
+            비밀번호 찾기
+          </Link>
         </form>
       </section>
     </main>
